55187aa28f5a7541bfc08d020889a33d4eebeef2
IGraciarena <ivanigraciarena@hotmail.com>
2020-05-22 17:37:02 -0300

    adding getMostCalledNumber

--- UTN-Phones/src/main/test/utn/dao/UserMySQLDaoTest.java
+++ UTN-Phones/src/main/test/utn/dao/UserMySQLDaoTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import utn.dao.mysql.UserMySQLDao;
-import utn.dao.mysql.MySQLUtils.*;
 import utn.model.User;
 import utn.model.enumerated.UserStatus;
 import utn.model.enumerated.UserType;
@@ -15,7 +14,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static utn.dao.mysql.MySQLUtils.GET_BY_USERNAME_USER_QUERY;
@@ -31,15 +29,16 @@ public class UserMySQLDaoTest {
     ResultSet rs;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         initMocks(this);
         dao = new UserMySQLDao(con);
     }
+
     @Test
     public void testGetByUserNameOk() throws SQLException {
         when(con.prepareStatement(GET_BY_USERNAME_USER_QUERY)).thenReturn(ps);
-        doNothing().when(ps).setString(1,"user");
-        doNothing().when(ps).setString(1,"pwd");
+        doNothing().when(ps).setString(1, "user");
+        doNothing().when(ps).setString(1, "pwd");
 
         when(ps.executeQuery()).thenReturn(rs);
         when(rs.next()).thenReturn(true);
@@ -57,18 +56,18 @@ public class UserMySQLDaoTest {
         doNothing().when(ps).close();
         doNothing().when(rs).close();
 
-        User u = dao.getByUserName("user","pwd");
+        User u = dao.getByUserName("user", "pwd");
 
-        assertEquals("username",u.getUsername());
-        assertEquals("pwd",u.getPwd());
-        assertEquals(Integer.valueOf(1),u.getId());
-        assertEquals(Integer.valueOf(2),u.getCity().getId());
-        assertEquals(Integer.valueOf(3),u.getCity().getProvince().getId());
-        assertEquals(userType,u.getUserType());
-        assertEquals(userStatus,u.getUserStatus());
+        assertEquals("username", u.getUsername());
+        assertEquals("pwd", u.getPwd());
+        assertEquals(Integer.valueOf(1), u.getId());
+        assertEquals(Integer.valueOf(2), u.getCity().getId());
+        assertEquals(Integer.valueOf(3), u.getCity().getProvince().getId());
+        assertEquals(userType, u.getUserType());
+        assertEquals(userStatus, u.getUserStatus());
 
 
-        verify(ps,times(2)).setString(anyInt(),anyString());
+        verify(ps, times(2)).setString(anyInt(), anyString());
 
     }
 }
