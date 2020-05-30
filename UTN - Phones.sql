67897e1715c318133c15e83fb418f48fbea149e5
Jorge Villordo <villordo235@gmail.com>
2020-05-30 20:51:27 -0300

    Update sql
    De todo perri, me puse la 10

--- UTN - Phones.sql
+++ UTN - Phones.sql
@@ -1,8 +1,9 @@
 create database utnphones;
 use utnphones;
 -- drop database utnphones;
-SET GLOBAL time_zone =  '-3:00';
 
+SET GLOBAL time_zone =  '-3:00';
+SET GLOBAL event_scheduler = ON;
 
 create table provinces(
 	id_province int auto_increment primary key,
@@ -12,7 +13,7 @@ create table provinces(
 create table cities(
 	id_city int not null auto_increment primary key,
 	city_name varchar(50),
-	prefix int,
+	prefix smallint(3) unsigned zerofill,
 	id_province_fk int,
 	constraint fk_province foreign key(id_province_fk) references provinces(id_province)
 );
@@ -44,7 +45,7 @@ create table users(
     
 create table user_lines(
 	id_user_line int not null auto_increment primary key,
-	line_number bigint unique key,
+	line_number bigint(10) unsigned zerofill unique key ,
 	type_line enum('MOBILE','RESIDENCE'),
 	line_status enum('ACTIVE','SUSPENDED','DELETE') default 'ACTIVE',
 	id_client_fk int,
@@ -54,10 +55,10 @@ create table user_lines(
 create table invoices(
 	id_invoice int not null auto_increment primary key,
 	call_count int,
-	price_cost decimal (12,2),
-	price_total decimal(12,2),
-	date_emission datetime,
-	date_expiration datetime,
+	total_cost decimal (12,2) ,
+	total_price decimal(12,2) ,
+	date_emission datetime ,
+	date_expiration datetime ,
 	invoice_status enum('PAID','NOT_PAID') default 'NOT_PAID',
 	id_line_fk int,
 	constraint fk_id_line foreign key (id_line_fk) references user_lines(id_user_line)
@@ -65,8 +66,8 @@ create table invoices(
 
 create table phonecalls(
 	id_phonecall int not null auto_increment primary key,
-	line_number_from varchar(40),
-    line_number_to varchar(40),
+	line_number_from varchar(10),
+    line_number_to varchar(10),
     id_line_number_from_fk int,
 	id_line_number_to_fk int,
 	id_city_from_fk int,
@@ -77,7 +78,7 @@ create table phonecalls(
  	price_per_min decimal (12,2),
  	total_price decimal (12,2),
  	total_cost decimal(12,2),
-	id_invoice_fk int,
+	id_invoice_fk int default null,
 	constraint fk_id_invoice foreign key (id_invoice_fk) references invoices(id_invoice),
     constraint fk_id_city_from foreign key (id_city_from_fk) references cities(id_city),
     constraint fk_id_city_to foreign key (id_city_to_fk) references cities(id_city),
@@ -90,15 +91,16 @@ insert into cities(city_name,prefix,id_province_fk)values("Mar del Plata","223",
 
 insert into users(first_name,surname,dni,birthdate,username,pwd,email,user_type,id_city_fk) values("Ivan","Graciarena","38704049",'1995-01-11',"ivanmdq22","123","ivanigraciarena@hotmail.com",1,1),("Juan","Garcia","36993049",'1990-03-30',"juancho","j2020","juang@hotmail.com",1,5),("Jorge","Villordo","39748321",'1983-10-17',"villordin","villoria","villord@hotmail.com",1,4),("Pedro","Mujica","203935789",'1940-11-22',"mijuquin","mijuaquin","pepe@hotmail.com",1,3),("Miguel","Granados","25090384",'1943-01-22',"miguelon","elguachon","miguegrana@hotmail.com",1,2);
 
-insert into user_lines (id_client_fk,line_number,type_line) values (1,"2235765132",1),(1,"2234770469",2),(2,"35115575392",1),(3,"3834918309",2),(4,"2918309532",1),(5,"011499899",2);
+insert into user_lines (id_client_fk,line_number,type_line) values (1,"2235765132",1),(1,"2234770469",2),(2,"3511557539",1),(3,"3834918309",2),(4,"2918309532",1),(5,"0114998997",2);
 
 insert into rates (price_per_min,cost_per_min,id_city_from_fk,id_city_to_fk)values(8,5,1,1),(8,5,2,2),(8,5,3,3),(8,5,4,4),(8,5,5,5),(8,5,6,6),(8,5,7,7),(8,5,8,8),(8,5,9,9),(9,5,1,2),(9,5,2,1),(10,5,1,3),(10,5,3,1),(9,5,1,4),(9,5,4,1),(9,7,1,5),(9,7,5,1),(15,10,1,6),(15,10,6,1),(14,11,1,7),(14,11,7,1),(10,8,1,8),(10,8,8,1),(8,6,1,9),(8,6,9,1),(10,5,2,3),(10,5,3,2),(9,5,2,4),(9,5,4,2),(9,7,2,5),(9,7,5,2),(15,10,2,6),(15,10,6,2),(14,11,2,7),(14,11,7,2),(10,8,2,8),(10,8,8,2),(8,6,2,9),(8,6,9,2),(9,5,3,4),(9,5,4,3),(9,7,3,5),(9,7,5,3),(15,10,3,6),(15,10,6,3),(14,11,3,7),(14,11,7,3),(10,8,3,8),(10,8,8,3),(8,6,3,9),(8,6,9,3),(9,7,4,5),(9,7,5,4),(15,10,4,6),(15,10,6,4),(14,11,4,7),(14,11,7,4),(10,8,4,8),(10,8,8,4),(8,6,4,9),(8,6,9,4),(15,10,5,6),(15,10,6,5),(14,11,5,7),(14,11,7,5),(10,8,5,8),(10,8,8,5),(8,6,5,9),(8,6,9,5),(14,11,6,7),(14,11,7,6),(10,8,6,8),(10,8,8,6),(8,6,6,9),(8,6,9,6),(10,8,7,8),(10,8,8,7),(8,6,7,9),(8,6,9,7),(8,6,8,9),(8,6,9,8);
 
-insert into invoices (call_count,date_emission,date_expiration,id_line_fk,price_cost,price_total)values(1,'2020-05-06','2020-05-21',1,200,350);
-insert into phonecalls(line_number_from,line_number_to,id_line_number_from_fk,id_line_number_to_fk,id_city_from_fk,id_city_to_fk,duration,call_date,id_invoice_fk)values("2235765132","11499899",1,6,1,2,20,'2020-04-03',1),("2235765132","11499899",1,6,1,3,20,'2020-01-05',1),("2235765132","11499899",1,6,1,5,20,'2020-04-03',1),(6,5,2,3,20,'2020-01-05',1),(6,3,2,5,20,'2020-04-05',1),(5,4,3,4,20,'2020-02-05',1),(6,3,2,5,20,'2020-04-05',1),(1,5,1,2,20,'2020-07-05',1),(4,6,4,2,20,'2020-02-05',1),(5,6,3,2,20,'2020-07-05',1);
+#insert into invoices (call_count,date_emission,date_expiration,id_line_fk,price_cost,price_total)values(1,'2020-05-06','2020-05-21',1,200,350);
+#insert into phonecalls(line_number_from,line_number_to,id_line_number_from_fk,id_line_number_to_fk,id_city_from_fk,id_city_to_fk,duration,call_date,id_invoice_fk)values("2235765132","11499899",1,6,1,2,20,'2020-04-03',1),("2235765132","11499899",1,6,1,3,20,'2020-01-05',1),("2235765132","11499899",1,6,1,5,20,'2020-04-03',1),(6,5,2,3,20,'2020-01-05',1),(6,3,2,5,20,'2020-04-05',1),(5,4,3,4,20,'2020-02-05',1),(6,3,2,5,20,'2020-04-05',1),(1,5,1,2,20,'2020-07-05',1),(4,6,4,2,20,'2020-02-05',1),(5,6,3,2,20,'2020-07-05',1);
 
 #//////////////////////////////ADD-PHONECALL//////////////////////////////////
-drop trigger TBI_ADD_PHONECALL
+#drop trigger TBI_ADD_PHONECALL;
+#select * from phonecalls;
 Delimiter //
 CREATE TRIGGER TBI_ADD_PHONECALL before insert on phonecalls for each row 
 begin
@@ -113,7 +115,7 @@ begin
     set vIdCityFrom=CALCULATE_ID_CITY(new.line_number_from);
     set vIdCityTo=CALCULATE_ID_CITY(new.line_number_to);
     set vIdLineFrom=CALCULATE_ID_LINE(new.line_number_from);
-    set vIdLineFrom=CALCULATE_ID_LINE(new.line_number_to);
+    set vIdLineTo=CALCULATE_ID_LINE(new.line_number_to);
     set vPricePerMin=CALCULATE_PRICE_PER_MIN(vIdCityFrom,vIdCityTo);
     set vCostPerMin=CALCULATE_COST_PER_MIN(vIdCityFrom,vIdCityTo);
     set vTotalCost=CALCULATE_TOTAL(new.duration,vCostPerMin);
@@ -186,9 +188,12 @@ begin
     return vReturn;
 end //
 #/////////////////////////////////////////////////////////////////
-insert into phonecalls(line_number_from,line_number_to,duration)values(2235765132,2918309532,45);
+insert into phonecalls(line_number_from,line_number_to,duration)values("0114998997","2918309532",35);
 select * from user_lines;
-select * from phonecalls;
+#select * from phonecalls;
+#select * from cities;
+delete from phonecalls
+
 
 #/////////////////////////////////////////////////////////////////
 SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;    
@@ -220,28 +225,12 @@ begin
 	end if;
     commit;
 end; //
-select * from user_lines;
-select * from invoices;
-drop procedure sp_add_user_line;
-delete from user_lines where id=8;
-delete from invoices where id=2;
-call sp_add_user_line(38704049,2235679563,1,2);
+#select * from user_lines;
+#select * from invoices;
+#drop procedure sp_add_user_line;
+#call sp_add_user_line(38704049,2235679563,1,2);
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 -- 
--- #falta hacer
--- #trigger para calcular el costo por minuto el precio por minuto de la tabla tarifas
--- create trigger tbi_cost_per_min before insert on fees for each row
--- begin 
--- 	
--- 	
--- 	
--- end
--- #trigger para calcular el date expiration +15 dias en invoices
--- create trigger tbi_date_expiration before insert on invoices for each row 
--- begin 
--- 	
--- 	
--- 	
--- end
 -- #trigger para calcular el price cost y price total en invoices
 -- create trigger tbi_total_price before insert on invoices for each row 
 -- begin 
@@ -256,62 +245,51 @@ call sp_add_user_line(38704049,2235679563,1,2);
 -- 	
 -- 	
 -- end
-
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # consulta llamadas de una usuario logueado por rango de fechas
-create procedure sp_phonecalls_betweendates(in pId int,in date_from date, in date_to date)
+Delimiter //
+create procedure sp_phonecalls_betweendates(in pId int,in date_from datetime, in date_to datetime)
 begin 
-	select p.id AS "ID",
+	select p.id_phonecall AS "ID",
 		   p.line_number_from AS "NUMBER FROM",
 		   p.line_number_to AS "NUMBER TO",
 		   p.duration AS "DURATION",
 		   p.call_date as "DATE",
 		   p.total_price as "TOTAL" 
 	from phonecalls as p 
-	join invoices as inv on p.id_invoice  = inv.id 
-	join user_lines as ul 	 on ul.id = inv.id_line 
+	inner join user_lines ul 
+    on p.id_line_number_from_fk=ul.id_user_line
+    inner join users u
+    on ul.id_client_fk=u.id_user
 	where p.call_date >= date_from 
 	and   p.call_date <= date_to
-	and   ul.id_client=Pid;
-end
+	and   u.id_user=Pid;
+end //
+#drop procedure sp_phonecalls_betweendates;
+#call sp_phonecalls_betweendates(1,'2020-05-23','2020-05-30 23:59:00');
 
-#consulta de los top 10 destinos mas llamados por el usuario
--- no andaaaaa
-drop procedure sp_user_top10;
-create procedure sp_user_top10(in pid int)
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#consulta de los top 5 destinos mas llamados por el usuario
+#drop procedure sp_user_top10;
+Delimiter //
+create procedure sp_user_top10(in pidUser int)
 begin
-	
-	select  ul.line_number as "OWNER",c.city_name as "CITY NAME",count(p.id_city_to)
-	from phonecalls as p
-	join cities as c on p.id_city_from = c.id 
-	join users as u on c.id = u.id_city
-	join user_lines as ul on u.id = ul.id_client 
-	where pid=u.id and ul.line_number = p.line_number_from 
-	group by c.city_name 
-	limit 10;
-	
-end
-
-# consulta de tarifas 
-create procedure sp_employee_fee(in pcity_from int)
-begin
-	select f.price_per_min as "PRICE"
-	from fees f
-	where pcity_from = f.id_city_from
-	order by f.price_per_min;
-end
-
-# consulta de llamada por usuario
-
-
-
-call sp_phonecalls_betweendates(1, '2020-02-01','2020-05-20');
-call sp_user_top10(1);
-
-select * from users;
-update users set user_status=2 where id=15;
-insert into users(first_name,surname,dni,birthdate,username,pwd,email,user_type,user_status,id_city) values("juan","choto","37989322",'1995-01-11',"juanchoto","123","juanchoto@hotmail.com",2,1,1);
-
-                          
+	select line_number_to
+	  from phonecalls as p
+      inner join user_lines ul 
+	  on p.id_line_number_from_fk=ul.id_user_line
+	  inner join users u
+	  on ul.id_client_fk=u.id_user
+	  where p.line_number_from=ul.line_number && u.id_user=pIdUser
+	  group by line_number_to
+	  order by count(line_number_to) desc
+	  limit 5;
+end //
+#call sp_user_top10(1);
+#select * from user_lines
+#insert into user_lines (line_number,type_line,line_status,id_client_fk)values('2236547876',1,1,6)
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#destino mas llamadi por el usuario(echo rancio)
 select f.line_number_to as "destino mas llamado" ,concat(u.first_name," ",u.surname) as "Nombre Apellido"
 from users as u 
 join (select line_number_to, id_line_number_from_fk
@@ -322,19 +300,112 @@ join (select line_number_to, id_line_number_from_fk
 	  limit 1) as f
 on f.id_line_number_from_fk = u.id_user;
 
-select * from user_lines;
-insert into phonecalls(line_number_from,line_number_to,id_line_number_from_fk,id_line_number_to_fk,id_city_from_fk,id_city_to_fk,duration,call_date,id_invoice_fk)values
-("2235765132","3834918309",1,4,1,2,20,'2020-04-03',1),("2235765132","3834918309",1,4,1,2,20,'2020-04-03',1),("2235765132","3834918309",1,4,1,2,20,'2020-04-03',1),("2235765132","3834918309",1,4,1,2,20,'2020-04-03',1)
-
-
-select * from users
-
-update users set user_type="EMPLOYEE" WHERE id_user = 1;
-
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#todas las llamadas de un usuario 
 select p.line_number_from,p.line_number_to,p.id_city_from_fk,p.id_city_to_fk,p.duration,p.call_date,p.total_price
 from phonecalls as p
 join user_lines as ul
 on p.id_line_number_from_fk=ul.id_user_line
 join users as u
 on ul.id_client_fk=u.id_user
-where u.id_user=1
+where u.id_user=1;
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+CREATE EVENT invoice_facturation
+ON SCHEDULE EVERY '1' MONTH
+STARTS '2020-05-01 00:00:00'
+DO
+CALL store_procedure();
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*y debe generar una
+factura por línea y debe tomar en cuenta todas las llamadas no facturadas
+para cada una de las líneas, sin tener en cuenta su fecha. La fecha de
+vencimiento de esta factura será estipulada a 15 días.*/
+Delimiter //
+create procedure sp_invoice_facturation()
+begin
+	
+end //
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+Delimiter //
+create procedure sp_create_invoices()
+begin
+	DECLARE finished INTEGER DEFAULT 0;
+	DECLARE id_userline int DEFAULT 0;
+
+	DECLARE curUserLines 
+		CURSOR FOR 
+			SELECT id_user_line FROM user_lines;
+
+	DECLARE CONTINUE HANDLER 
+        FOR NOT FOUND SET finished = 1;
+
+	OPEN curUserLines;
+
+	getUserLine: LOOP
+		FETCH curUserLines INTO id_userline;
+		IF finished = 1 THEN 
+			LEAVE getUserLine;
+		END IF;
+        insert into invoices(id_line_fk)VALUES(id_userline);
+	END LOOP getUserLine;
+	CLOSE curUserLines;
+end //
+call sp_create_invoices();
+select * from phonecalls
+select * from invoices;
+delete from invoices;
+delete from phonecalls;
+#//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Delimiter //
+create function CALCULATE_CALLCOUNT(pIdLine int)
+returns int
+reads sql data
+begin
+	declare vReturn int;
+    set vReturn=(select count(*) from phonecalls where id_line_number_from_fk=pIdLine && id_invoice_fk is null);
+	return vReturn;
+end //
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Delimiter //
+create function CALCULATE_TOTAL_COST(pIdLine int)
+returns int
+reads sql data
+begin
+	declare vReturn int;
+    set vReturn=(select sum(total_cost) from phonecalls where id_line_number_from_fk=pIdLine);
+    return vReturn;
+end //
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Delimiter //
+create function CALCULATE_TOTAL_PRICE(pIdLine int)
+returns int
+reads sql data
+begin
+	declare vReturn int;
+    set vReturn=(select sum(total_price) from phonecalls where id_line_number_from_fk=pIdLine);
+    return vReturn;
+end //
+#//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Delimiter //
+create trigger tbi_calculate_invoice_data before insert on invoices for each row
+begin
+	declare vCallcount int;
+    declare vTotalCost int;
+    declare vTotalPrice int;
+    declare vDateExp datetime;
+    set vCallCount=CALCULATE_CALLCOUNT(new.id_line_fk);
+    set vTotalCost=CALCULATE_TOTAL_COST(new.id_line_fk);
+    set vTotalPrice=CALCULATE_TOTAL_PRICE(new.id_line_fk);
+    set new.date_emission=now();
+    set new.date_expiration=DATE_ADD(NOW(),INTERVAL 15 DAY);
+    set new.total_price=vTotalPrice;
+    set new.total_cost=vTotalCost;
+    set new.call_count=vCallCount;
+end //
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////77
+Delimiter //
+create trigger tai_addphonecall_to_invoice after insert on invoices for each row
+begin
+	update phonecalls set id_invoice_fk=new.id_invoice where id_line_number_from_fk=new.id_line_fk;
+end //
